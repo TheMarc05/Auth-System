4e7da46b8d3d2b32e3581602837952e8c9317411
TheMarc05 <corneamarc@gmail.com>
2025-08-22 23:05:26 +0300

    finished database setup

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.send("signup controller");
+};
+
+export const login = async (req, res) => {
+  res.send("login controller");
+};
+
+export const logout = async (req, res) => {
+  res.send("logout controller");
+};
