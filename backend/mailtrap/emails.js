d3ad6ca17b74ad15306c76202c05cae598b20e50
TheMarc05 <corneamarc@gmail.com>
2025-08-29 17:10:37 +0300

    forgot password endpoint

--- backend/mailtrap/emails.js
+++ backend/mailtrap/emails.js
@@ -1,5 +1,8 @@
 import { mailtrapClient, sender } from "./mailtrap.config.js";
-import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+import {
+  PASSWORD_RESET_REQUEST_TEMPLATE,
+  VERIFICATION_EMAIL_TEMPLATE,
+} from "./emailTemplates.js";
 
 export const sendVerificationEmail = async (email, verificationToken) => {
   const recipient = [{ email }];
@@ -41,5 +44,22 @@ export const sendWelcomeEmail = async (email, name) => {
   } catch (error) {
     console.log(`Error sending welcome email: ${error}`);
     throw new Error(`Error sending welcome email: ${error}`);
+  }
+};
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+  const recipient = [{ email }];
+
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      subject: "Reset your Password",
+      html: PASSWORD_RESET_REQUEST_TEMPLATE.replace("{resetURL}", resetURL),
+      category: "Password reset",
+    });
+  } catch (error) {
+    console.log(`Error sending password reset email: ${error}`);
+    throw new Error(`Error sending password reset email: ${error}`);
   }
 };
