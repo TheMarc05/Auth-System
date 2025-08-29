81eeea850a9f0500523c43c06cad999ec2506a01
TheMarc05 <corneamarc@gmail.com>
2025-08-29 15:01:44 +0300

    login endpoint

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -87,7 +87,40 @@ export const verifyEmail = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  res.send("login controller");
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    const isPasswordCorrect = await bcryptjs.compare(password, user.password);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    if (!isPasswordCorrect) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid credentials" });
+    }
+
+    generateTokenAndSetCookie(res, user._id);
+
+    user.lastLogin = new Date();
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Logged in successfully",
+      user: {
+        ...user._doc,
+        password: undefined,
+      },
+    });
+  } catch (error) {
+    console.log("Error in login", error);
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const logout = async (req, res) => {
